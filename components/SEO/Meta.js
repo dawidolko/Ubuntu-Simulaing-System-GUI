7a6a5f06a720482ac838caf4d17cd171fd204c9a
Dawid Olko <dawid_olko@outlook.com>
2026-02-15 11:35:19 +0100

    Refactor components for consistency and readability

--- components/SEO/Meta.js
+++ components/SEO/Meta.js
@@ -4,7 +4,6 @@ import Head from "next/head";
 export default function Meta() {
   return (
     <Head>
-      /* Primary Meta Tags */
       <title>ubuntu.dawidolko.pl - Simulaing System Ubuntu</title>
       <meta charSet="utf-8" />
       <meta
@@ -25,9 +24,7 @@ export default function Meta() {
       <meta name="language" content="English" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#E95420" />
-      /* Search Engine */
       <meta name="image" content="images/logos/fevicon.png" />
-      /* Schema.org for Google */
       <meta
         itemProp="name"
         content="ubuntu.dawidolko.pl - Simulaing System Ubuntu"
@@ -37,7 +34,6 @@ export default function Meta() {
         content="dawidolko's (dawidolko) Personal Portfolio Website. Made with Ubuntu 20.4 (Linux) theme by Next.js and Tailwind CSS."
       />
       <meta itemProp="image" content="images/logos/fevicon.png" />
-      /* Twitter */
       <meta name="twitter:card" content="summary" />
       <meta
         name="twitter:title"
@@ -50,7 +46,6 @@ export default function Meta() {
       <meta name="twitter:site" content="dawidolko" />
       <meta name="twitter:creator" content="dawidolko" />
       <meta name="twitter:image:src" content="images/logos/logo_1024.png" />
-      /* Open Graph general (Facebook, Pinterest & Google+) */
       <meta
         name="og:title"
         content="ubuntu.dawidolko.pl - Simulaing System Ubuntu"
--- components/apps/vscode.js
+++ components/apps/vscode.js
@@ -7,8 +7,6 @@ export default function VsCode() {
       frameBorder="0"
       title="VsCode"
       className="h-full w-full bg-ub-cool-grey"></iframe>
-    // this is not my work, but it's amazing!
-    // Here is the link to the original repo: https://github.com/conwnet/github1s
   );
 }
 
